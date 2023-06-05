e96a43b73ea496396ff01199866a1f4c10ec300f
Chizhara <85157536+Chizhara@users.noreply.github.com>
2023-06-05 18:45:06 +0300

    Revert "feat: Добавить классы FilmControllers и UserControllers"

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/NegativeDurationException.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.filmorate.exceptions;
-
-public class NegativeDurationException extends RuntimeException {
-    public NegativeDurationException(String message, int duration) {
-        super(message + ": " + duration);
-    }
-}
--- src/main/java/ru/yandex/practicum/filmorate/exceptions/OutOfRangeFilmDescriptionException.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.filmorate.exceptions;
-
-public class OutOfRangeFilmDescriptionException extends RuntimeException {
-    public OutOfRangeFilmDescriptionException(String message, int exceptedValue, int receivedValue) {
-        super(message + ". Ожидаемое: " + exceptedValue + "; Полученное: " + receivedValue);
-    }
-}
